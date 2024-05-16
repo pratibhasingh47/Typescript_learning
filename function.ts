954182b39971d34000cae37f6422a122052c888f
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-05-17 00:15:46 +0530

    update.ts

--- function.ts
+++ function.ts
@@ -46,5 +46,11 @@ function consoleError(errmsg : string) : void{
     console.log(errmsg);
 }
 
+
+function handleError(errmsg : string) : never{
+    throw new Error(errmsg);
+    
+}
+
 export{}
 
