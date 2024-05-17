d8844c2f3b17aa4bd72d65c96df6c7fd4b10d3c7
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-05-18 00:18:43 +0530

    update.ts

--- array.ts
+++ array.ts
@@ -8,6 +8,15 @@ type User = {
     isActive:boolean
 }
 
+const allUsers : User[] = []
+
+const MLModels : number[][] = [
+    [255,255,255],
+    []
+]
+
 
 superhero.push("Bat-man")
 heropower.push(123)
+
+allUsers.push({Name : "hello",isActive:true});
