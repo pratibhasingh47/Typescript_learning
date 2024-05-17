527efc53f1537d15110110ab743e3004f7362633
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-05-18 00:15:23 +0530

    update.ts

---
+++ array.ts
@@ -0,0 +1,13 @@
+const superhero :string []= []
+
+// const heropower :number []= []
+const heropower : Array<number> = []
+
+type User = {
+    Name:string
+    isActive:boolean
+}
+
+
+superhero.push("Bat-man")
+heropower.push(123)
