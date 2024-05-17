1b1dee18704a6268079ed0c7ecfba94b2692f832
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-05-18 00:45:13 +0530

    update.ts

--- tuples.ts
+++ tuples.ts
@@ -1,5 +1,11 @@
 // const User: (string | number)[] = ["ps" ,1]
 
-let tuser: [string,number,boolean]
+let tuser: [string, number, boolean]
 
-tuser = ["ps" , 131 , true]
+tuser = ["ps", 131, true]
+
+let rgb: [number, number, number] = [255, 255, 255]
+
+type user = [number, string]
+
+// const newuser :user [112 , "eg.com"]
