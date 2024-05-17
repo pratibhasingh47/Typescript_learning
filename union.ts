81fb4e3d755f61c6975befd372d707636ee600a4
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-05-18 00:27:53 +0530

    update.ts

---
+++ union.ts
@@ -0,0 +1,25 @@
+let score : number | string  = 47
+
+score = 61
+score = "54"
+
+type Users ={
+    Name : string;
+    id : number
+}
+
+type Admin ={
+    userName : string;
+    id : number
+}
+
+let pratibha : Users | Admin ={
+    Name : "Pratibha",
+    id : 147
+}
+
+pratibha = {userName :"debug" , id:147}
+
+function getdbid(id : number | string){
+    console.log()
+}
