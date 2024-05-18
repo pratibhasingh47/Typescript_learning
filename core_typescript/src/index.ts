9f0c9876fdd5f33a5a66439ef3d61d66561574a3
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-05-19 00:14:22 +0530

    update.ts

--- core_typescript/src/index.ts
+++ core_typescript/src/index.ts
@@ -1,15 +1,15 @@
 console.log("Typescript is here");
 console.log("Typescript is still here");
 
-class user1{
-    email : string
-    name : string
-    city : string =""
-    constructor(email:string , name : string){
+class user1 {
+    email: string
+    name: string
+    city: string = ""
+    constructor(email: string, name: string) {
         this.email = email;
         this.name = name
     }
 }
 
-const ps = new user1("ps.com" ,"prats")
-ps.city ="udaipur";
+const ps = new user1("ps.com", "prats")
+ps.city = "udaipur";
