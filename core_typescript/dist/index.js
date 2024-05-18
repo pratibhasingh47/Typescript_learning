1125bc2f150742238b2f0bf13b33899d5ba51f53
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-05-19 00:17:54 +0530

    update.ts

--- core_typescript/dist/index.js
+++ core_typescript/dist/index.js
@@ -1,2 +1,12 @@
 "use strict";
 console.log("Typescript is here");
+console.log("Typescript is still here");
+class user1 {
+    constructor(email, name) {
+        this.city = "";
+        this.email = email;
+        this.name = name;
+    }
+}
+const ps = new user1("ps.com", "prats");
+ps.city = "udaipur";
