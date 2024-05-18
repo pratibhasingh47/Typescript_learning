8962583cc5f51203ca2f7c9e8b41f4bd0ddbf0c8
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-05-19 00:13:54 +0530

    update.ts

--- interface.ts
+++ interface.ts
@@ -1,47 +1,47 @@
-interface Userr{
-    readonly dbID : number
-    email : string,
-    userID : number,
-    googleId : string
+interface Userr {
+    readonly dbID: number
+    email: string,
+    userID: number,
+    googleId: string
     // startTrail : ()=> string
-    starttrail ():string
-    getCoupon(couponname:string):number
+    starttrail(): string
+    getCoupon(couponname: string): number
 
 }
 
-interface Userr{
-    githubToken : string
+interface Userr {
+    githubToken: string
 }
 
-interface Adminn extends Userr{
-    role: "admin"|"ta"|"student"
+interface Adminn extends Userr {
+    role: "admin" | "ta" | "student"
 }
 
-const Pratibha : Userr ={
-    dbID : 41,
-    email : "ps.com",
-    userID : 123,
-    googleId : "ps@.com",
-    starttrail:()=>{
+const Pratibha: Userr = {
+    dbID: 41,
+    email: "ps.com",
+    userID: 123,
+    googleId: "ps@.com",
+    starttrail: () => {
         return "trail started"
     },
-    getCoupon:(name : "pratibha")=>{
+    getCoupon: (name: "pratibha") => {
         return 10
     },
-    githubToken :"ps47",
+    githubToken: "ps47",
 }
 
-const pratibha_singh : Adminn ={
-    role : "student",
-    dbID : 414,
-    email : "ps.com",
-    userID : 123,
-    googleId : "ps@.com",
-    starttrail:()=>{
+const pratibha_singh: Adminn = {
+    role: "student",
+    dbID: 414,
+    email: "ps.com",
+    userID: 123,
+    googleId: "ps@.com",
+    starttrail: () => {
         return "trail started"
     },
-    getCoupon:(name : "pratibha")=>{
+    getCoupon: (name: "pratibha") => {
         return 10
     },
-    githubToken :"ps47",
+    githubToken: "ps47",
 }
