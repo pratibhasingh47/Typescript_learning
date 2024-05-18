9fd1d95516c620ab9af0120639e80179b8a60fc2
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-05-18 23:08:27 +0530

    update.ts

---
+++ enums.ts
@@ -0,0 +1,13 @@
+// const AISLE = 0
+// const MIDDLE = 1
+// const WINDOW = 2
+
+enum SeatChoice {
+    AISLE = "aisle",
+    MIDDLE = "middle",
+    WINDOW = "window",
+    FOURTH = 3,
+    FIFTH
+}
+
+const seat1 = SeatChoice.AISLE
--- union.ts
+++ union.ts
@@ -1,35 +1,35 @@
-let score : number | string  = 47
+let score: number | string = 47
 
 score = 61
 score = "54"
 
-type Users ={
-    Name : string;
-    id : number
+type Users = {
+    Name: string;
+    id: number
 }
 
-type Admin ={
-    userName : string;
-    id : number
+type Admin = {
+    userName: string;
+    id: number
 }
 
-let pratibha : Users | Admin ={
-    Name : "Pratibha",
-    id : 147
+let pratibha: Users | Admin = {
+    Name: "Pratibha",
+    id: 147
 }
 
-pratibha = {userName :"debug" , id:147}
+pratibha = { userName: "debug", id: 147 }
 
-function getdbid(id : number | string){
+function getdbid(id: number | string) {
     console.log(`DB id is : ${id}`)
 
-    if(typeof id == "string"){
+    if (typeof id == "string") {
         id.toLowerCase()
     }
 }
 
 
-const data: (number | string )[] = [1,2,3,"4"]
+const data: (number | string)[] = [1, 2, 3, "4"]
 
 let pi: 3.14 = 3.14
 // pi=3.15
