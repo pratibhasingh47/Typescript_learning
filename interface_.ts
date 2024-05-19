9b5194d8d55b64355ec970184f8266439ec327ee
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-05-20 00:28:20 +0530

    update.ts

--- interface_.ts
+++ interface_.ts
@@ -1,5 +1,22 @@
 interface TakePhoto{
     cameraMode : string
     filter : string
-    burst : string
+    burst : number
+}
+
+class Instagram implements TakePhoto{
+    constructor(
+        public cameraMode: string,
+        public filter: string,
+        public burst : number
+    ){}
+}
+
+class Youtube implements TakePhoto{
+    constructor(
+        public cameraMode: string,
+        public filter: string,
+        public burst : number,
+        public shorts:string
+    ){}
 }
