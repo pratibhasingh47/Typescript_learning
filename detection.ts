1d958a8261b08c3473df9d30bab7f30e9087d936
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-05-21 22:56:21 +0530

    update.ts

--- detection.ts
+++ detection.ts
@@ -51,3 +51,22 @@ function logValue(x: Date | string) {
         // (parameter) x: string
     }
 }
+
+
+type Fish = { swim: () => void };
+type Bird = { fly: () => void };
+
+function isFish(pet: Fish | Bird): pet is Fish {
+    return (pet as Fish).swim !== undefined;
+}
+
+function getFood(pet : Fish|Bird){
+    if(isFish(pet)){
+        pet
+        return "Fish Food"
+    }
+    else{
+        pet
+        return "Bird Food"
+    }
+}
