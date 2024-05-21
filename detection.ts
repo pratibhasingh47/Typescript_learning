761d09424255bf4a5685a95780b024c540af9d8d
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-05-21 23:21:18 +0530

    update.ts

--- detection.ts
+++ detection.ts
@@ -72,44 +72,44 @@ function getFood(pet: Fish | Bird) {
 }
 
 
-interface Circle{
-    kind :"circle",
-    radius : number
+interface Circle {
+    kind: "circle",
+    radius: number
 }
 
-interface Square{
-    kind :"square",
-    side:number
+interface Square {
+    kind: "square",
+    side: number
 }
 
-interface Rectangle{
-    kind :"reactangle",
-    lenght:number,
-    width:number
+interface Rectangle {
+    kind: "reactangle",
+    lenght: number,
+    width: number
 }
 
-type Shape = Circle|Square|Rectangle
+type Shape = Circle | Square | Rectangle
 
-function getTrueShape(shape:Shape){
-    if(shape.kind ==="circle"){
-        return Math.PI * shape.radius**2
+function getTrueShape(shape: Shape) {
+    if (shape.kind === "circle") {
+        return Math.PI * shape.radius ** 2
     }
     // return shape.side * shape.side
 }
 
-function getArea(shape:Shape){
-    switch(shape.kind){
-        case "circle" :
-            return Math.PI * shape.radius**2
+function getArea(shape: Shape) {
+    switch (shape.kind) {
+        case "circle":
+            return Math.PI * shape.radius ** 2
 
-        case "square" :
+        case "square":
             return shape.side * shape.side
 
         case "reactangle":
             return shape.lenght * shape.width
 
-        default :
-            const _defaultshape:never = shape
+        default:
+            const _defaultshape: never = shape
             return _defaultshape
     }
 }
