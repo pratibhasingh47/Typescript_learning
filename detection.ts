9878faed354aaa89e509abf935fd1f9aa83e903d
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-05-21 23:07:16 +0530

    update.ts

--- detection.ts
+++ detection.ts
@@ -70,3 +70,29 @@ function getFood(pet: Fish | Bird) {
         return "Bird Food"
     }
 }
+
+
+interface Circle{
+    kind :"circle",
+    radius : number
+}
+
+interface Square{
+    kind :"square",
+    side:number
+}
+
+interface Rectangle{
+    kind :"reactangle",
+    lenght:number,
+    width:number
+}
+
+type Shape = Circle|Square
+
+function getTrueShape(shape:Shape){
+    if(shape.kind ==="circle"){
+        return Math.PI * shape.radius**2
+    }
+    return shape.side * shape.side
+}
