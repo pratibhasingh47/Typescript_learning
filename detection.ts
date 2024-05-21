dd2e6984ca2234ad9411a831a5a7defacb9019f1
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-05-21 23:19:05 +0530

    update.ts

--- detection.ts
+++ detection.ts
@@ -88,11 +88,28 @@ interface Rectangle{
     width:number
 }
 
-type Shape = Circle|Square
+type Shape = Circle|Square|Rectangle
 
 function getTrueShape(shape:Shape){
     if(shape.kind ==="circle"){
         return Math.PI * shape.radius**2
     }
-    return shape.side * shape.side
+    // return shape.side * shape.side
+}
+
+function getArea(shape:Shape){
+    switch(shape.kind){
+        case "circle" :
+            return Math.PI * shape.radius**2
+
+        case "square" :
+            return shape.side * shape.side
+
+        case "reactangle":
+            return shape.lenght * shape.width
+
+        default :
+            const _defaultshape:never = shape
+            return _defaultshape
+    }
 }
