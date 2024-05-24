bc8a6bd82ccf825dc209e50373bf6abfec44a63b
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-05-25 00:45:02 +0530

    update.ts

--- Objects.ts
+++ Objects.ts
@@ -115,4 +115,24 @@ class Point {
 const pt = new Point();
 console.log(`${pt.x}, ${pt.y}`);
 
+function PrintCoord(pt: { x: number; y: number }) {
+    console.log("The coordinate's x value is " + pt.x);
+    console.log("The coordinate's y value is " + pt.y);
+}
+printCoord({ x: 3, y: 7 });
+
+
+type point = {
+    x: number;
+    y: number;
+};
+
+// Exactly the same as the earlier example
+function printCoord(pt: Point) {
+    console.log("The coordinate's x value is " + pt.x);
+    console.log("The coordinate's y value is " + pt.y);
+}
+
+printCoord({ x: 100, y: 100 });
+
 export { }
